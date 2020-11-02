e27219eb85d0c70b28aad5cde5ae8eb173fe2560
dedow <73851243+dedow@users.noreply.github.com>
2020-11-03 02:13:08 +0700

    init

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteName: `Using TypeScript Example`,
+    exampleUrl: `https://github.com/gatsbyjs/gatsby/tree/master/examples/using-typescript`,
+  },
+  plugins: [
+    `gatsby-plugin-postcss`
+  ],
+}
